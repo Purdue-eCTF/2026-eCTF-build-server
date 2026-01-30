4d6cb15165265b298966ffe44e9fbc05290ecc3a
guantonamous <rolandocahill@gmail.com>
2026-01-30 17:37:30 -0500

    removed distro comments from main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 from builder import init_build_queue
 from connection import serve
-# from distribution import init_distribution_queue
 
 if __name__ == "__main__":
     init_build_queue()
-    # init_distribution_queue()
     serve()
